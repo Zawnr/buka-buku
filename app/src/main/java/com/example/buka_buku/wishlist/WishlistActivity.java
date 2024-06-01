62b2c0fd716fd52ae1b66318507b6882958d0179
ila <ilavirnandaf@student.ub.ac.id>
2024-06-01 19:44:22 +0700

    feat: add wishlist layout - Created wishlist layout - Added header LinearLayout - Implemented RecyclerView for wishlist items - Included checkout ImageButton

---
+++ app/src/main/java/com/example/buka_buku/wishlist/WishlistActivity.java
@@ -0,0 +1,26 @@
+package com.example.buka_buku.wishlist;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.buka_buku.R;
+
+public class WishlistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_wishlist);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
