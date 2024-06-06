8f02b8a2d977a11c149afd1662d4376398f5bc2f
alfina zahra <alfinaz916@gmail.com>
2024-06-06 17:13:21 +0700

    feat: add layout home

---
+++ app/src/main/java/com/example/buka_buku/HomeModel.java
@@ -0,0 +1,12 @@
+package com.example.buka_buku;
+
+public class HomeModel {
+    int image;
+    String title, autor;
+
+    public HomeModel(int image, String title, String autor){
+        this.image = image;
+        this.title = title;
+        this.autor = autor;
+    }
+}
