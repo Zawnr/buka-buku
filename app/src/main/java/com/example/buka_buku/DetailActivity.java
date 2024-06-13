5ac6871a39f9c35942bb84d8678e82faeefb6eee
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-06-13 17:03:20 +0700

    feat: fetch detail activity data from home activity intent

--- app/src/main/java/com/example/buka_buku/DetailActivity.java
+++ app/src/main/java/com/example/buka_buku/DetailActivity.java
@@ -1,6 +1,11 @@
 package com.example.buka_buku;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.example.buka_buku.model.Book;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private ImageView bookCover;
+    private TextView bookTitle;
+    private TextView bookAuthor;
+    private ImageButton btnBack;
+    private TextView bookDescription;
+    private TextView bookRating;
+    private TextView bookGenre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,33 @@ public class DetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bookCover = findViewById(R.id.image);
+        bookTitle = findViewById(R.id.title);
+        bookAuthor = findViewById(R.id.writer);
+        btnBack = findViewById(R.id.btn_back);
+        bookDescription = findViewById(R.id.description);
+        bookRating = findViewById(R.id.rating);
+        bookGenre = findViewById(R.id.genre);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Intent intent = getIntent();
+        Book book = (Book) intent.getSerializableExtra("book");
+
+        if (book != null) {
+            bookTitle.setText(book.getTitle());
+            bookAuthor.setText(book.getAuthor());
+            bookDescription.setText(book.getDescription());
+            bookRating.setText(book.getRating());
+            bookGenre.setText(book.getGenre());
+            Glide.with(this).load(book.getCover()).into(bookCover);
+        }
     }
 }
