a59290f53affe1737678cf48d90bb655baa4a96b
alfina zahra <alfinaz916@gmail.com>
2024-06-13 14:04:38 +0700

    feat: add get image from storage realtime database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,10 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-auth")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 }
