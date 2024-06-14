68a2a06e4d74504f00432e7ea38004668ba7bbdf
ila <ilavirnandaf@student.ub.ac.id>
2024-06-14 17:28:38 +0700

    Fetch book data from Firebase Storage and add checkout navigation

--- app/src/main/java/com/example/buka_buku/WishlistAdapter.java
+++ app/src/main/java/com/example/buka_buku/WishlistAdapter.java
@@ -1,5 +1,6 @@
 package com.example.buka_buku;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,54 +8,42 @@ import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.buka_buku.model.Book;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 public class WishlistAdapter extends RecyclerView.Adapter<WishlistAdapter.WishlistViewHolder> {
 
     private List<Book> wishlistItems;
+    private Context context;
+    private DatabaseReference databaseReference;
 
     public WishlistAdapter(List<Book> wishlistItems) {
         this.wishlistItems = wishlistItems;
+        this.databaseReference = FirebaseDatabase.getInstance("https://buka-buku-919aa-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
     }
 
     @NonNull
     @Override
     public WishlistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_wishlist, parent, false);
+        context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_item_wishlist, parent, false);
         return new WishlistViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WishlistViewHolder holder, int position) {
-        Book item = wishlistItems.get(position);
-        holder.bookTitle.setText(item.getTitle());
-        holder.genre.setText(item.getGenre());
-        holder.author.setText(item.getAuthor());
-
-        holder.removeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int currentPosition = holder.getAdapterPosition();
-                if (currentPosition != RecyclerView.NO_POSITION) {
-                    wishlistItems.remove(currentPosition);
-                    notifyItemRemoved(currentPosition);
-                    notifyItemRangeChanged(currentPosition, wishlistItems.size());
-                }
-            }
-        });
-
-//        holder.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-//            int currentPosition = holder.getAdapterPosition();
-////            if (currentPosition != RecyclerView.NO_POSITION) {
-////                wishlistItems.get(currentPosition).setChecked(isChecked);
-////            }
-//        });
+        Book book = wishlistItems.get(position);
+        holder.bind(book);
     }
 
     @Override
@@ -62,22 +51,62 @@ public class WishlistAdapter extends RecyclerView.Adapter<WishlistAdapter.Wishli
         return wishlistItems.size();
     }
 
-    public static class WishlistViewHolder extends RecyclerView.ViewHolder {
-//        CheckBox checkBox;
-//        ImageView bookCover;
-        TextView bookTitle;
-        TextView genre;
-        TextView author;
+    class WishlistViewHolder extends RecyclerView.ViewHolder {
+        CheckBox checkBox;
+        ImageView bookCover;
+        TextView bookTitle, genre, author;
         ImageButton removeButton;
 
-        public WishlistViewHolder(@NonNull View itemView) {
+        WishlistViewHolder(View itemView) {
             super(itemView);
-//            checkBox = itemView.findViewById(R.id.checkBox);
-//            bookCover = itemView.findViewById(R.id.book_cover);
+            checkBox = itemView.findViewById(R.id.checkBox);
+            bookCover = itemView.findViewById(R.id.book_cover);
             bookTitle = itemView.findViewById(R.id.book_title);
             genre = itemView.findViewById(R.id.genre);
             author = itemView.findViewById(R.id.author);
             removeButton = itemView.findViewById(R.id.remove);
+
+            // Setup event click untuk tombol hapus
+            removeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        Book book = wishlistItems.get(position);
+                        removeBookFromWishlist(book);
+                    }
+                }
+            });
+        }
+
+        void bind(Book book) {
+            bookTitle.setText(book.getTitle());
+            genre.setText(book.getGenre());
+            author.setText(book.getAuthor());
+            Glide.with(bookCover.getContext()).load(book.getCover()).into(bookCover); // Load gambar dari URL
+
+            // Set initial state of checkbox
+            checkBox.setChecked(book.isSelected());
+
+            // Handle checkbox state change
+            checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> book.setSelected(isChecked));
+        }
+
+
+        void removeBookFromWishlist(Book book) {
+            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            DatabaseReference bookRef = databaseReference.child("wishlist").child(userId).child(book.getKey());
+
+            bookRef.removeValue().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    wishlistItems.remove(getAdapterPosition());
+                    notifyItemRemoved(getAdapterPosition());
+                    notifyItemRangeChanged(getAdapterPosition(), wishlistItems.size());
+                    Toast.makeText(context, "Book removed from wishlist", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Failed to remove book", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
